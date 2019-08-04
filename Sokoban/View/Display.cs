775cc5ac4c997b005b4c9f8eecbe27d143cd104f
boqnd <boyandon@gmail.com>
2019-08-04 23:27:39 +0300

    Print border around the field

--- Sokoban/View/Display.cs
+++ Sokoban/View/Display.cs
@@ -8,9 +8,15 @@ namespace Sokoban.View
         public void DisplayField(Field field,Player player,Obsticales obsticales, Boxes boxes)
         {
             string currCoordinate = "";
-            
+
+            for (int col = 0; col < field.Cols+2; col++)
+            {
+                Console.Write("▒ ");
+            }
+            Console.WriteLine();
             for (int row = 0; row < field.Rows; row++)
             {
+                Console.Write("▒ ");
                 for (int col = 0; col < field.Cols; col++)
                 {
                     currCoordinate = row + " " + col;
@@ -29,15 +35,20 @@ namespace Sokoban.View
                     }
                     else if (obsticales.Coordinates.Contains(currCoordinate))
                     {
-                        Console.Write("  ");
+                        Console.Write("▒ ");
                     }
                     else
                     {
-                        Console.Write("▒ ");
+                        Console.Write("  ");
                     } 
                 }
+                Console.Write("▒ ");
 
                 Console.WriteLine();
+            }
+            for (int col = 0; col < field.Cols+2; col++)
+            {
+                Console.Write("▒ ");
             }
         }
     }
