c4e2e25d4f16e1fad3451a3a2ed9a7ee2910948c
boqnd <boyandon@gmail.com>
2019-08-04 23:36:58 +0300

    Player movement

--- Sokoban/Controller/Service.cs
+++ Sokoban/Controller/Service.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Sokoban.Model;
 using Sokoban.View;
 
@@ -23,8 +25,42 @@ namespace Sokoban.Controller
                 new List<string>(){"4 4","4 3"}
                 );
             _display = new Display();
-            
             _display.DisplayField(field,player,obsticales,boxes);
+            Play();
+        }
+
+        public void Play()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                char input = Console.ReadKey().KeyChar;
+
+                int playerRow = player.Coordinates.Split().Select(int.Parse).ToList()[0];
+                int playerCol = player.Coordinates.Split().Select(int.Parse).ToList()[1];
+                
+                switch (input)
+                {
+                    case 'w':
+                        playerRow--;
+                        break;
+                    case 'a':
+                        playerCol--;
+                        break;
+                    case 's':
+                        playerRow++;
+                        break;
+                    case 'd':
+                        playerCol++;
+                        break;
+                }
+                
+                
+
+                player.Coordinates = playerRow + " " + playerCol;
+                
+                _display.DisplayField(field,player,obsticales,boxes);
+            }
         }
     }
 }
