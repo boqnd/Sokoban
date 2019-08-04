6e09aef66a497d6a569a913a0e30ec97fc04d002
boqnd <boyandon@gmail.com>
2019-08-04 23:14:02 +0300

    Update View

---
+++ Sokoban/View/Display.cs
@@ -0,0 +1,42 @@
+using System;
+using Sokoban.Model;
+
+namespace Sokoban.View
+{
+    public class Display
+    {
+        void DisplayField(Field field,Player player,Obsticales obsticales, Boxes boxes)
+        {
+            string currCoordinate = "";
+            
+            for (int row = 0; row < field.Rows; row++)
+            {
+                for (int col = 0; col < field.Cols; col++)
+                {
+                    currCoordinate = row + " " + col;
+                    
+                    if (currCoordinate == player.Coordinates)
+                    {
+                        Console.WriteLine("& ");
+                    }
+                    else if (boxes.BoxCoordinates.Contains(currCoordinate))
+                    {
+                        Console.WriteLine("# ");
+                    }
+                    else if (boxes.GoalCoordinates.Contains(currCoordinate))
+                    {
+                        Console.WriteLine("X ");
+                    }
+                    else if (obsticales.Coordinates.Contains(currCoordinate))
+                    {
+                        Console.WriteLine("  ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("▒ ");
+                    } 
+                }
+            }
+        }
+    }
+}
